549eb22c30cfcc11653438393820686a9beccf0d
Steven Li <ilumii@users.noreply.github.com>
2019-11-13 14:10:19 -0500

    maps

--- components/Routes.js
+++ components/Routes.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
-import Home from '../pages/Home.js/index.js'
-import About from '../pages/About.js/index.js'
+import Home from '../pages/Home.js'
+import About from '../pages/About.js'
+import Manhattan from '../pages/manhattan.js'
+import Queens from '../pages/queens.js'
+import Brooklyn from '../pages/brooklyn.js'
+import Bronx from '../pages/bronx.js'
+import StatenIsland from '../pages/statenIsland.js'
 
 const Routes = () => (
    <Router>
       <Scene key = "root">
          <Scene key = "home" component = {Home} title = "Home" initial = {true} />
          <Scene key = "about" component = {About} title = "About" />
+         <Scene key = "Manhattan" component = {Manhattan} title = "Manhattan" />
+         <Scene key = "Queens" component = {Queens} title = "Queens" />
+         <Scene key = "Brooklyn" component = {Brooklyn} title = "Brooklyn" />
+         <Scene key = "Bronx" component = {Bronx} title = "Bronx" />
+         <Scene key = "StatenIsland" component = {StatenIsland} title = "Staten Island" />
       </Scene>
    </Router>
 )
---
+++ pages/statenIsland.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { TouchableOpacity, Text, Dimensions, StyleSheet, View } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import axios from 'axios';
+
+export default class Manhattan extends React.Component {
+	_isMounted = false;
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
+
+	async componentDidMount() {
+		this._isMounted = true;
+	}
+
+	componentWillUnmount() {
+		this._isMounted = false;
+    }
+
+	onMapReady = () => this.setState({ marginBottom: 0 })
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<MapView 
+					onMapReady={this.onMapReady}
+					style={[styles.map, { flex: 1, marginBottom: this.state.marginBottom }]}
+					initialRegion={{
+						latitude: 40.7549,
+						longitude: -73.9840,
+						latitudeDelta: 0.05,
+						longitudeDelta: 0.05
+					}}
+					showsCompass={false}
+					loadingEnabled={true}>
+				</MapView>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0,
+		justifyContent: 'flex-end',
+		alignItems: 'center'
+	},
+	map: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0
+	}
+});
