0ed020f4be3f58f06b4fced56113c593eb3a7118
Toms <chen.tommy925@gmail.com>
2019-11-20 18:06:09 -0500

    footer tab

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import  Icon  from 'react-native-vector-icons/Ionicons'
+import { Container, Footer, FooterTab, Button } from 'native-base';
 import Routes from './components/Routes.js'
 
 class reactTutorialApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activePage: 1
+    };
+  }
+
   render() {
     return (
-      <Routes />
+      <Container>
+        <Routes />
+        <Footer>
+          <FooterTab>
+            <Button onPress={()=>this.setState({activePage:1})}>
+              <Icon style={this.state.activePage === 1 ? { color: '#1E90FF', fontSize: 20 } : { fontSize: 20 }} name="ios-subway" />
+            </Button>
+            <Button onPress={()=>this.setState({activePage:2})}>
+              <Icon style={this.state.activePage === 2 ? { color: '#1E90FF', fontSize: 20 } : { fontSize: 20 }} active name="ios-map" />
+            </Button>
+            <Button onPress={()=>this.setState({activePage:3})}>
+              <Icon style={this.state.activePage === 3 ? { color: '#1E90FF', fontSize: 20 } : { fontSize: 20 }} name="logo-twitter" type="Ionicons" />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     )
   }
 }
